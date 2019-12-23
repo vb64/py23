0d71072c8bf125cdfe715080ace3adb4e3b6362a
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2019-12-23 11:49:52 +0400

    test load_module_by_path

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,6 +1,7 @@
 """
 __init__.py tests
 """
+import os
 import pytest
 
 
@@ -11,3 +12,12 @@ def test_remove_symbols():
     from py23 import remove_symbols
 
     assert remove_symbols('c:\\win/file.txt', '/\\:.') == 'cwinfiletxt'
+
+
+def test_load_module_by_path():
+    """
+    load_module_by_path
+    """
+    from py23 import load_module_by_path
+
+    assert callable(load_module_by_path(os.path.join('py23', '__init__.py')).remove_symbols)
