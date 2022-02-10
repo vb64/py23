6e667b6c438b28a7fa1644f98755a5b0a9f5502f
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2022-02-10 15:36:53 +0400

    add functions

--- py23/__init__.py
+++ py23/__init__.py
@@ -36,3 +36,55 @@ def load_module_by_path(path):
             py_module = imp.load_source(name, path)
 
     return py_module
+
+
+def win1251(text):
+    """Convert to 1251 encoding."""
+    return to_str(text).encode('windows-1251')
+
+
+def to_str(text):
+    """Convert to unicode."""
+    try:  # Python 2
+        return text.decode('utf-8')
+    except AttributeError:  # pragma: no cover
+        # Python 3.5+
+        return text
+
+
+def is_contains(text_1251, utf8_string):
+    """Check text with 1251 encoding contains string with utf-8 encoding."""
+    try:  # Python 2
+        return win1251(utf8_string) in text_1251
+    except TypeError:  # pragma: no cover
+        # Python 3.5+
+        return win1251(utf8_string) in text_1251.encode('windows-1251')
+
+
+def replace1251(text_1251, utf8_source, utf8_dest):
+    """Replace utf8 source string to utf8 destination string in 1251 encoding text."""
+    try:  # Python 2
+        return text_1251.replace(win1251(utf8_source), win1251(utf8_dest))
+    except TypeError:  # pragma: no cover
+        # Python 3.5+
+        return text_1251.encode('windows-1251').replace(win1251(utf8_source), win1251(utf8_dest))
+
+
+def gen_next(generator):
+    """Next method for generator."""
+    try:  # Python 2
+        return generator.next()
+    except AttributeError:  # pragma: no cover
+        # Python 3.5+
+        return generator.__next__()
+
+
+def open_text_file(file_path, mode, encoding):
+    """Open text file with encoding."""
+    try:  # Python 3.5+
+        fhandle = open(file_path, mode + 't', encoding=encoding)
+    except TypeError:  # pragma: no cover
+        # Python 2
+        fhandle = open(file_path, mode + 'b')  # pylint: disable=unspecified-encoding
+
+    return fhandle
--- tests/test_init.py
+++ tests/test_init.py
@@ -1,23 +1,38 @@
-"""
-__init__.py tests
-"""
+"""Tests __init__.py module."""
 import os
 import pytest
 
 
 def test_remove_symbols():
-    """
-    remove_symbols
-    """
+    """Check remove_symbols function."""
     from py23 import remove_symbols
 
     assert remove_symbols('c:\\win/file.txt', '/\\:.') == 'cwinfiletxt'
 
 
 def test_load_module_by_path():
-    """
-    load_module_by_path
-    """
+    """Check load_module_by_path function."""
     from py23 import load_module_by_path
 
     assert callable(load_module_by_path(os.path.join('py23', '__init__.py')).remove_symbols)
+
+
+def test_win1251():
+    """Check win1251 function."""
+    from py23 import win1251
+
+    assert win1251(u'test') == 'test'
+
+
+def test_open_text_file():
+    """Check win1251 function."""
+    from py23 import open_text_file
+
+    assert open_text_file('README.md', 'r', 'utf-8')
+
+
+def test_gen_next():
+    """Check gen_next function."""
+    from py23 import gen_next
+
+    assert gen_next((i for i in range(2))) is not None
